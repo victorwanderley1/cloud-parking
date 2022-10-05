0da7cd169988030df4107f4f870f9c24d565ca2f
victorwanderley1 <victor.wanderley1709@gmail.com>
2022-10-05 20:35:28 -0300

    draft: Changing ParkingController's methods.

--- src/main/java/com/vw/dev/parking/controller/ParkingController.java
+++ src/main/java/com/vw/dev/parking/controller/ParkingController.java
@@ -1,26 +1,32 @@
 package com.vw.dev.parking.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vw.dev.parking.controller.dto.ParkingDTO;
+import com.vw.dev.parking.controller.mapper.ParkingMapper;
 import com.vw.dev.parking.entity.Parking;
+import com.vw.dev.parking.service.ParkingService;
 
 @RestController
 @RequestMapping("/parking")
 public class ParkingController {
+	
+	private final ParkingService parkingService;
+	private final ParkingMapper parkingMapper;
+	
+	public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+		this.parkingService = parkingService;
+		this.parkingMapper = parkingMapper;
+	}
 
 	@GetMapping
-	public List<Parking> findAll(){
-		var parking = new Parking();
-		parking.setColor("Preto");
-		parking.setLicense("AS57I2");
-		parking.setModel("GM Corsa");
-		parking.setState("RN");
-		
-		return Arrays.asList(parking); 
+	public List<ParkingDTO> findAll(){
+		List<Parking> parkings = parkingService.findAll();
+		List<ParkingDTO> parkingDTOs = parkingMapper.toParkingDTOList(parkings);
+		return parkingDTOs;
 	}
 }
