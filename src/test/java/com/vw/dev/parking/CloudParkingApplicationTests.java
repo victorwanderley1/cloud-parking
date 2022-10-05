58419ebeb954f9608c998f8ac1913b3bd621ab20
victorwanderley1 <victor.wanderley1709@gmail.com>
2022-10-04 21:38:24 -0300

    draft: First Commit, initial files.

---
+++ src/test/java/com/vw/dev/parking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vw.dev.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
