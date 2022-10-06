32f9bd98ec4d8701dd712c7da94837f68f3cbeb7
victorwanderley1 <victor.wanderley1709@gmail.com>
2022-10-06 19:50:23 -0300

    draft: creating ParkingController's tests

---
+++ src/test/java/com/vw/dev/parking/controller/ParkingControllerTests.java
@@ -0,0 +1,114 @@
+package com.vw.dev.parking.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import com.vw.dev.parking.controller.dto.ParkingCreateDTO;
+
+import io.restassured.RestAssured;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ParkingControllerTests {
+
+	@LocalServerPort
+	private int randomPort;
+
+	@BeforeEach
+	void setUpTest() {
+		RestAssured.port = randomPort;
+	}
+
+	@Test
+	void whenFindAllCheckResult() {
+		ParkingCreateDTO parkingCreateDTO = new ParkingCreateDTO();
+		parkingCreateDTO.setLicense("WM233K2");
+		parkingCreateDTO.setModel("Corsa");
+		parkingCreateDTO.setColor("Verde");
+		parkingCreateDTO.setState("RN");
+
+		RestAssured.given().when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE)
+				.post("/parking");
+
+		RestAssured.given().when().get("/parking").then().statusCode(HttpStatus.OK.value()).body("license",
+				Matchers.hasItem("AB123C4"));
+	}
+
+	@Test
+	void whenFindByIdCheckResult() {
+		ParkingCreateDTO parkingCreateDTO = new ParkingCreateDTO();
+		parkingCreateDTO.setLicense("WM233K2");
+		parkingCreateDTO.setModel("Corsa");
+		parkingCreateDTO.setColor("Verde");
+		parkingCreateDTO.setState("RN");
+
+		String id = RestAssured.given().when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE)
+				.post("/parking").then().extract().body().jsonPath().get("id");
+
+		RestAssured.given().get("/parking/{id}", id).then().statusCode(HttpStatus.OK.value()).body("id",
+				Matchers.equalTo(id));
+	}
+	
+	@Test
+	void whenFindByIdCheckException() {
+		String id = "123";
+		RestAssured.given().get("/parking/{id}", id).then().statusCode(HttpStatus.NOT_FOUND.value());
+	}
+
+	@Test
+	void whenCreateParkingThenCheckResult() {
+		ParkingCreateDTO parkingCreateDTO = new ParkingCreateDTO();
+		parkingCreateDTO.setLicense("AB123C4");
+		parkingCreateDTO.setModel("Corcel");
+		parkingCreateDTO.setColor("Laranja");
+		parkingCreateDTO.setState("PI");
+
+		RestAssured.given().when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE).post("/parking")
+				.then().statusCode(HttpStatus.CREATED.value()).body("license", Matchers.equalTo("AB123C4"))
+				.body("color", Matchers.equalTo("Laranja"));
+	}
+
+	@Test
+	void whenDeleteThenCheckResult() {
+		ParkingCreateDTO parkingCreateDTO = new ParkingCreateDTO();
+		parkingCreateDTO.setLicense("WM233K2");
+		parkingCreateDTO.setModel("Corsa");
+		parkingCreateDTO.setColor("Verde");
+		parkingCreateDTO.setState("RN");
+		
+		String id = RestAssured.given()
+				.when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE).post("/parking")
+				.then().extract().body().jsonPath().get("id");
+		
+		RestAssured.given()
+			.when().delete("/parking/{id}", id)
+			.then().statusCode(HttpStatus.NO_CONTENT.value());
+	}
+	
+	@Test
+	void whenUpdateThenCheckResult() {
+		ParkingCreateDTO parkingCreateDTO = new ParkingCreateDTO();
+		parkingCreateDTO.setLicense("WM233K2");
+		parkingCreateDTO.setModel("Corsa");
+		parkingCreateDTO.setColor("Verde");
+		parkingCreateDTO.setState("RN");
+		
+		String id = RestAssured.given()
+				.when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE).post("/parking")
+				.then().extract().body().jsonPath().get("id");
+		
+		parkingCreateDTO.setColor("Verde Lodo");
+		
+		RestAssured.given()
+			.when().contentType(MediaType.APPLICATION_JSON_VALUE).body(parkingCreateDTO)
+			.put("/parking/{id}", id)
+			.then().statusCode(HttpStatus.OK.value())
+			.body("color", Matchers.equalTo("Verde Lodo"));
+	}
+}
