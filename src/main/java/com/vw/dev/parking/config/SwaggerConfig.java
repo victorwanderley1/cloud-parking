a500db3d0e37ed4dc1dc3ae640ad320f67fb193c
victorwanderley1 <victor.wanderley1709@gmail.com>
2022-10-08 16:21:34 -0300

    draft: Adding SpringSecurity Configurations and Classes

--- src/main/java/com/vw/dev/parking/config/SwaggerConfig.java
+++ src/main/java/com/vw/dev/parking/config/SwaggerConfig.java
@@ -1,12 +1,20 @@
 package com.vw.dev.parking.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
@@ -15,9 +23,22 @@ public class SwaggerConfig {
 	public Docket getDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select().apis(RequestHandlerSelectors.basePackage("com.vw.dev.parking.controller"))
-				.build().apiInfo(getMetaData());
+				.build().apiInfo(getMetaData()).securityContexts(Arrays.asList(getSecurityContext()))
+				.securitySchemes(Arrays.asList(basicScheme()));
 	}
 	
+	private SecurityContext getSecurityContext() {
+		return SecurityContext.builder().securityReferences(Arrays.asList(basicAuthReference())).build();
+	}
+
+	private SecurityReference basicAuthReference() {
+		return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+	}
+	
+	private SecurityScheme basicScheme() {
+		return new BasicAuth("basicAuth");
+	}
+
 	private ApiInfo getMetaData() {
 		return new ApiInfoBuilder()
 				.title("Cloud Parking")
--- src/test/java/com/vw/dev/parking/controller/ParkingControllerTests.java
+++ src/test/java/com/vw/dev/parking/controller/ParkingControllerTests.java
@@ -1,14 +1,10 @@
 package com.vw.dev.parking.controller;
 
-import java.time.LocalDateTime;
-
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -16,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import com.vw.dev.parking.controller.dto.ParkingCreateDTO;
-import com.vw.dev.parking.repository.ParkingRepository;
 
 import io.restassured.RestAssured;
 
@@ -26,18 +21,11 @@ class ParkingControllerTests extends AbstractContainerBase{
 
 	@LocalServerPort
 	private int randomPort;
-	@Autowired
-	private ParkingRepository repository;
 	@BeforeEach
 	void setUpTest() {
 		RestAssured.port = randomPort;
 	}
 	
-	@AfterAll
-	void finallyTests() {
-		repository.deleteAll();
-	}
-
 	@Test
 	void whenFindAllCheckResult() {
 		ParkingCreateDTO parkingCreateDTO = new ParkingCreateDTO();
@@ -46,10 +34,10 @@ class ParkingControllerTests extends AbstractContainerBase{
 		parkingCreateDTO.setColor("Verde");
 		parkingCreateDTO.setState("RN");
 
-		RestAssured.given().when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE)
+		RestAssured.given().auth().basic("user","abc@vw@123").when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE)
 				.post("/parking");
 
-		RestAssured.given().when().get("/parking").then().statusCode(HttpStatus.OK.value()).body("license",
+		RestAssured.given().auth().basic("user","abc@vw@123").when().get("/parking").then().statusCode(HttpStatus.OK.value()).body("license",
 				Matchers.hasItem("HJ236R3"));
 	}
 
@@ -61,17 +49,17 @@ class ParkingControllerTests extends AbstractContainerBase{
 		parkingCreateDTO.setColor("Branco");
 		parkingCreateDTO.setState("BA");
 
-		String id = RestAssured.given().when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE)
+		String id = RestAssured.given().when().auth().basic("user","abc@vw@123").body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE)
 				.post("/parking").then().extract().body().jsonPath().get("id");
 
-		RestAssured.given().get("/parking/{id}", id).then().statusCode(HttpStatus.OK.value()).body("id",
+		RestAssured.given().when().auth().basic("user","abc@vw@123").get("/parking/{id}", id).then().statusCode(HttpStatus.OK.value()).body("id",
 				Matchers.equalTo(id));
 	}
 	
 	@Test
 	void whenFindByIdCheckException() {
 		String id = "123";
-		RestAssured.given().get("/parking/{id}", id).then().statusCode(HttpStatus.NOT_FOUND.value());
+		RestAssured.given().when().auth().basic("user","abc@vw@123").get("/parking/{id}", id).then().statusCode(HttpStatus.NOT_FOUND.value());
 	}
 
 	@Test
@@ -82,7 +70,7 @@ class ParkingControllerTests extends AbstractContainerBase{
 		parkingCreateDTO.setColor("Laranja");
 		parkingCreateDTO.setState("PI");
 
-		RestAssured.given().when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE).post("/parking")
+		RestAssured.given().when().auth().basic("user","abc@vw@123").body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE).post("/parking")
 				.then().statusCode(HttpStatus.CREATED.value()).body("license", Matchers.equalTo("AB123C4"))
 				.body("color", Matchers.equalTo("Laranja"));
 	}
@@ -96,11 +84,11 @@ class ParkingControllerTests extends AbstractContainerBase{
 		parkingCreateDTO.setState("RN");
 		
 		String id = RestAssured.given()
-				.when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE).post("/parking")
+				.when().auth().basic("user","abc@vw@123").body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE).post("/parking")
 				.then().extract().body().jsonPath().get("id");
 		
 		RestAssured.given()
-			.when().delete("/parking/{id}", id)
+			.when().auth().basic("user","abc@vw@123").delete("/parking/{id}", id)
 			.then().statusCode(HttpStatus.NO_CONTENT.value());
 	}
 	
@@ -113,13 +101,13 @@ class ParkingControllerTests extends AbstractContainerBase{
 		parkingCreateDTO.setState("RN");
 		
 		String id = RestAssured.given()
-				.when().body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE).post("/parking")
+				.when().auth().basic("user","abc@vw@123").body(parkingCreateDTO).contentType(MediaType.APPLICATION_JSON_VALUE).post("/parking")
 				.then().extract().body().jsonPath().get("id");
 		
 		parkingCreateDTO.setColor("Verde Lodo");
 		
 		RestAssured.given()
-			.when().contentType(MediaType.APPLICATION_JSON_VALUE).body(parkingCreateDTO)
+			.when().auth().basic("user","abc@vw@123").contentType(MediaType.APPLICATION_JSON_VALUE).body(parkingCreateDTO)
 			.put("/parking/{id}", id)
 			.then().statusCode(HttpStatus.OK.value())
 			.body("color", Matchers.equalTo("Verde Lodo"));
@@ -134,16 +122,18 @@ class ParkingControllerTests extends AbstractContainerBase{
 		parkingCreateDTO.setState("RN");
 		
 		String id = RestAssured.given()
-		.when().contentType(MediaType.APPLICATION_JSON_VALUE)
+		.when().auth().basic("user","abc@vw@123").contentType(MediaType.APPLICATION_JSON_VALUE)
 		.body(parkingCreateDTO).post("/parking")
 		.then().statusCode(HttpStatus.CREATED.value())
 		.extract().body().jsonPath().get("id");
 		
 		RestAssured.given()
-		.when().param("id", id).patch("/parking")
+		.when().auth().basic("user","abc@vw@123").param("id", id).patch("/parking")
 		.then().statusCode(HttpStatus.OK.value())
 		.body("bill", Matchers.notNullValue())
 		.and().body("exitDate", Matchers.notNullValue());
 		
 	}
+	
+	
 }
