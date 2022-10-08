16c9d8268e59e9188969eea04698599abe1ef8e3
victorwanderley1 <victor.wanderley1709@gmail.com>
2022-10-07 22:34:05 -0300

    draft: Adding Service Aux classes

---
+++ src/main/java/com/vw/dev/parking/service/ParkingServiceAux.java
@@ -0,0 +1,61 @@
+package com.vw.dev.parking.service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import com.vw.dev.parking.entity.Parking;
+
+@Component
+public class ParkingServiceAux {
+	private static ParkingServiceAux parkingServiceAux;
+	private static int ONE_HOUR = 60;
+	private static int TWENTY_FOUR_HOUR = 24*ONE_HOUR;
+	private static Double ONE_HOUR_VALUE = 5.00;
+	private static Double ADDITIONAL_PER_HOUR_VALUE = 2.00;
+	private static Double DAY_VALUE = 20.00;
+	private Parking parking;
+	
+	public static ParkingServiceAux getInstance() {
+		if(parkingServiceAux == null) {
+			parkingServiceAux = new ParkingServiceAux();
+		}
+		return parkingServiceAux;
+	}
+	
+	public Parking checkOut(Parking parking) {
+		return this.setParking(parking).setTimeExit().generateBill().getParking();
+	}
+	
+	private ParkingServiceAux generateBill() {
+		parking.setBill(calcBill(Duration.between(parking.getEntryDate(), parking.getExitDate()).toMinutes()));
+		return this;
+	}
+	
+	private Double calcBill(Long minutes) {
+		if(minutes <= ONE_HOUR) {
+			return ONE_HOUR_VALUE;
+		}else if (minutes <= TWENTY_FOUR_HOUR){
+			int hours = (int) (minutes/ONE_HOUR);
+			return (Double) ONE_HOUR_VALUE+(ADDITIONAL_PER_HOUR_VALUE*(hours));
+		}else {
+			int days = (int) (minutes/TWENTY_FOUR_HOUR);
+			return (Double) DAY_VALUE*days;
+		}
+	}
+	private ParkingServiceAux setTimeExit() {
+		this.parking.setExitDate(LocalDateTime.now());
+		return this;
+	}
+	
+	private ParkingServiceAux setParking(Parking parking) {
+		this.parking = parking;
+		return this;
+	}
+	
+	private Parking getParking() {
+		return parking;
+	}
+}
---
+++ src/test/java/com/vw/dev/parking/service/ParkingServiceAuxTests.java
@@ -0,0 +1,13 @@
+package com.vw.dev.parking.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ParkingServiceAuxTests {
+	
+	@Test
+	void checkOutTest(){
+		
+	}
+}
